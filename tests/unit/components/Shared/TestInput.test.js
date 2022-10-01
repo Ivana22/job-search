95d0190023119c6ff987b47d3c553f7a397569b4
ivanatrpkova <ivana.trpkova@codeflair.ch>
2022-10-01 07:44:59 +0200

    TestInput unit tests added

---
+++ tests/unit/components/Shared/TestInput.test.js
@@ -0,0 +1,17 @@
+import { mount } from "@Vue/test-utils";
+import TextInput from "@/components/Shared/TextInput";
+
+describe("TextInput", () => {
+  it("communicates that user has entered characted", () => {
+    const wrapper = mount(TextInput, {
+      props: {
+        modelValue: "",
+      },
+    });
+    const input = wrapper.find("input");
+    input.setValue("NY");
+    input.setValue("NYC");
+    const messages = wrapper.emitted()["update:modelValue"];
+    expect(messages).toEqual([["NY"], ["NYC"]]);
+  });
+});

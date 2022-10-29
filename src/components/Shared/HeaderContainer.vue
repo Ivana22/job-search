049a20727d4eaba7c7150660bdca1e9018f245ce
ivanatrpkova <ivana.trpkova@codeflair.ch>
2022-10-29 10:00:45 +0200

    create HeaderContainer component and add new Teams view

---
+++ src/components/Shared/HeaderContainer.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="w-full border-b border-solid border-brand-gray-1">
+    <div class="mx-auto my-16 text-center">
+      <slot name="title">Sample fallback Title</slot>
+      <slot name="subTitle">Samle fallback subTitle</slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HeaderContainer",
+};
+</script>
